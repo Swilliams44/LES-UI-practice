349b527dd1ed16981b6e1c4691e0af2ad2779180
Selena P Williams <Selena.Williams@firstsource.com>
2026-04-14 11:22:44 -0400

    Update test_document_library.py
    Edited to make the BASE_URL = "https://LES-SPW-Git-Practice.com"

--- test_document_library.py
+++ test_document_library.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 # Constants (set to your environment)
-BASE_URL = "https://your-app-url.com"
+BASE_URL = "https://LES-SPW-Git-Practice.com"
 USERNAME = "clientuser"
 PASSWORD = "password"
 DOWNLOAD_DIR = "/tmp/downloads"
